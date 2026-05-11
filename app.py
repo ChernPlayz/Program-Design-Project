8fe98ca0f45d40812dc2a28108ac49542bd1b000
Chern <chernplayzofficial@gmail.com>
2026-05-11 19:21:04 +0800

    Debugging

--- app.py
+++ app.py
@@ -6,15 +6,26 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+# https://program-design-project.onrender.com/
+@app.route('/')
+def home():
+  return "Flask backend is running :D"
+
+# https://program-design-project.onrender.com/get-backend-data
 @app.route('/get-backend-data')
 def get_data():
-  result = subprocess.check_output(["./main"])
-
-  # Send data back to JS as JSON
-  return jsonify({
+  try:
+    result = subprocess.check_output(["./main"])
+    # Send data back to JS as JSON
+    return jsonify({
       "status": "success",
-      "output": result.decode('utf-8')
-  })
+      "output": result.decode('utf-8'),
+    })
+  except Exception as err:
+    return jsonify({
+      "status": "error",
+      "message": str(err),
+    })
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
