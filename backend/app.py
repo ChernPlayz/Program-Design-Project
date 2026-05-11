99a95007bd00e1c9813dd7a2fe9b9f16ce9c665e
Chern <chernplayzofficial@gmail.com>
2026-05-11 18:04:50 +0800

    Port updated

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 # Lets Python run external programs like C++ executables
 import subprocess
+import os
 
 # Create the Flask application
 app = Flask(__name__)
@@ -25,4 +26,4 @@ def get_data():
 
 if __name__ == "__main__":
   # Run server on port 5000, URL becomes: http://127.0.0.1:5000
-  app.run(debug=True, port=5000)
+  app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
